6bd52f54e3a37d6ced957972d077d0ce6f87a6db
Fiegsy <yassine.daoui031@gmail.com>
2024-03-08 13:30:28 -0800

    Updated console part to make more readable

--- console.py
+++ console.py
@@ -1,104 +1,103 @@
+import cmd
+from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
-from models import storage
 
-class Console:
-    def do_create(self, args):
-        """Creates a new instance of a given class"""
-        if not args:
-            print("** class name missing **")
-            return
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
 
-        class_name, *attr_args = args.split()
-        if class_name not in globals():
-            print("** class doesn't exist **")
-            return
-
-        cls = globals()[class_name]
-        new_instance = cls()
-        storage.save()
-        print(new_instance.id)
-
-    def do_show(self, args):
-        """Shows the string representation of an instance"""
-        if not args:
-            print("** class name missing **")
-            return
-
-        class_name, instance_id = args.split()
-        if class_name not in globals():
-            print("** class doesn't exist **")
-            return
-
-        instance_key = f"{class_name}.{instance_id}"
-        if instance_key not in storage.all():
-            print("** no instance found **")
-            return
-
-        print(storage.all()[instance_key])
-
-    def do_destroy(self, args):
-        """Deletes an instance"""
-        if not args:
-            print("** class name missing **")
-            return
-
-        class_name, instance_id = args.split()
-        if class_name not in globals():
-            print("** class doesn't exist **")
-            return
-
-        instance_key = f"{class_name}.{instance_id}"
-        if instance_key not in storage.all():
-            print("** no instance found **")
-            return
-
-        del storage.all()[instance_key]
-        storage.save()
-
-    def do_all(self, args):
-        """Shows all instances of a class"""
-        if not args:
-            print([str(storage.all()[id]) for id in storage.all()])
-            return
-
-        if args not in globals():
-            print("** class doesn't exist **")
-            return
-
-        instances = [str(storage.all()[id]) for id in storage.all() if args in id]
-        print(instances)
-
-    def do_update(self, args):
-        """Updates an instance attribute"""
-        if not args:
-            print("** class name missing **")
-            return
-
-        class_name, instance_id, attr_name, attr_value = args.split()
-        if class_name not in globals():
-            print("** class doesn't exist **")
-            return
-
-        instance_key = f"{class_name}.{instance_id}"
-        if instance_key not in storage.all():
-            print("** no instance found **")
-            return
-
-        setattr(storage.all()[instance_key], attr_name, attr_value)
-        storage.save()
+    def __init__(self, storage):
+        super().__init__()
+        self.storage = storage
 
     def do_quit(self, arg):
-        """Exits the console"""
         return True
 
     def do_EOF(self, arg):
-        """Exits the console with EOF (Ctrl+D)"""
         return True
 
     def emptyline(self):
-        """Handles empty line input"""
         pass
 
-if __name__ == "__main__":
-    console = Console()
-    console.cmdloop()
+    def do_create(self, args):
+        if not args:
+            print("** class name missing **")
+            return
+
+        cls_name, *attr_args = args.split()
+        cls = getattr(__import__('models'), cls_name, None)
+        if not cls:
+            print("** class doesn't exist **")
+            return
+
+        new_instance = cls()
+        self.storage.save()
+        print(new_instance.id)
+
+    def do_show(self, args):
+        if not args:
+            print("** class name missing **")
+            return
+
+        class_name, instance_id = args.split()
+        instance_key = f"{class_name}.{instance_id}"
+        instance = self.storage.all().get(instance_key)
+        if not instance:
+            print("** no instance found **")
+            return
+
+        print(instance)
+
+    def do_destroy(self, args):
+        if not args:
+            print("** class name missing **")
+            return
+
+        class_name, instance_id = args.split()
+        instance_key = f"{class_name}.{instance_id}"
+        instances = self.storage.all()
+        if instance_key not in instances:
+            print("** no instance found **")
+            return
+
+        del instances[instance_key]
+        self.storage.save()
+
+    def do_all(self, args):
+        if args:
+            class_name = args.split()[0]
+            instances = [str(instance) for key, instance in self.storage.all().items() if key.startswith(class_name)]
+            print(instances)
+        else:
+            print([str(instance) for instance in self.storage.all().values()])
+
+    def do_update(self, args):
+        if not args:
+            print("** class name missing **")
+            return
+
+        list_args = args.split()
+        if len(list_args) < 2:
+            print("** instance id missing **")
+            return
+
+        instance_key = f"{list_args[0]}.{list_args[1]}"
+        instances = self.storage.all()
+        if instance_key not in instances:
+            print("** no instance found **")
+            return
+
+        if len(list_args) < 3:
+            print("** attribute name missing **")
+            return
+
+        if len(list_args) < 4:
+            print("** value missing **")
+            return
+
+        setattr(instances[instance_key], list_args[2], list_args[3])
+        self.storage.save()
+
+if __name__ == '__main__':
+    storage = FileStorage()
+    storage.reload()
+    HBNBCommand(storage).cmdloop()
