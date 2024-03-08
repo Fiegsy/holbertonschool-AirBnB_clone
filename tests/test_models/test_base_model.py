824c2668ee9a6a08a841eb357cc3a3c4e41bf3c0
Fiegsy <yassine.daoui031@gmail.com>
2024-03-08 07:43:44 -0800

    Fixed the dict method again

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -10,22 +10,13 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key == 'id':
                     self.id = value
-                elif key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key == 'created_at':
+                    self.created_at = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 else:
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
-
-    def __str__(self):
-        """Return a string representation of the BaseModel instance."""
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__,
-            self.id,
-            self.__dict__
-        )
 
     def save(self):
         """Update the updated_at attribute with the current datetime."""
@@ -33,9 +24,12 @@ class BaseModel:
 
     def to_dict(self):
         """Return a dictionary representation of the BaseModel instance."""
-        return {
-            "__class__": self.__class__.__name__,
-            "id": self.id,
-            "created_at": self.created_at.isoformat(),
-            "updated_at": self.updated_at.isoformat()
-        }
+        dict_copy = self.__dict__.copy()
+        dict_copy['created_at'] = self.created_at.isoformat()
+        if hasattr(self, 'updated_at'):
+            dict_copy['updated_at'] = self.updated_at.isoformat()
+        return dict_copy
+
+    def __str__(self):
+        """Return a string representation of the BaseModel instance."""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
